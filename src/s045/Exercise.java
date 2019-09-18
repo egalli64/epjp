c0a9aece0e72320b7b142205d1abafdf6c801c3a
egalli64 <egalli64@gmail.com>
2019-09-18 10:44:38 +0200

    more soulutions (with an error)

--- src/s045/Exercise.java
+++ src/s045/Exercise.java
@@ -7,7 +7,10 @@ public class Exercise {
         exercise("Bob");
         exercise("ciAo");
 
-        System.out.println("My toUpper(): " + toUpper("Bob"));
+        System.out.println("toUpper(): " + toUpper("Bob"));
+        System.out.println("toUpper2(): " + toUpper2("Bob"));
+        System.out.println("toUpper3(): " + toUpper3("Bob"));
+        System.out.println("toUpper4(): " + toUpper4("Bob"));
     }
 
     public static String toUpper(String s) {
@@ -25,6 +28,48 @@ public class Exercise {
         return sb.toString();
     }
 
+    public static String toUpper2(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                sb.setCharAt(i, (char)(cur-GAP));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String toUpper3(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                sb.insert(i, cur);
+            } else {
+                sb.insert(i, cur);                
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String toUpper4(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                String x = Character.toString((char)(cur - GAP));
+                sb.replace(i, i+1, x);
+            }
+        }
+
+        return sb.toString();
+    }
+    
     public static void exercise(String sample) {
         System.out.println(sample + " to uppercase and lowercase");
         for (int i = 0; i < sample.length(); i++) {
