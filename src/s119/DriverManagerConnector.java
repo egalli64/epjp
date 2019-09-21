4ce9f376e020e804c1839a049986517d4caa3f2c
Manny <egalli64@gmail.com>
2019-09-21 11:51:19 +0200

    MySql reference added

--- src/s119/DriverManagerConnector.java
+++ src/s119/DriverManagerConnector.java
@@ -5,14 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverManagerConnector {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";
+//    private static final String URL = "jdbc:mysql://localhost:3306/me?serverTimezone=Europe/Rome";
     private static final String USER = "me";
     private static final String PASSWORD = "password";
 
     public static void main(String[] args) {
         try {
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected as " + conn.getSchema());
+
+            String user = conn.getSchema();
+            if (user == null) {
+                user = conn.getCatalog();
+            }
+
+            System.out.println("Connected as " + user);
         } catch (SQLException e) {
             e.printStackTrace();
         }
