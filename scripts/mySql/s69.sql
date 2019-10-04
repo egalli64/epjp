ed604a04981dee2ae2ea19ef2ef7b46b9df6da51
egalli64 <egalli64@gmail.com>
2019-10-04 15:43:10 +0200

    simple procedure w/ parameters

--- scripts/mySql/s69.sql
+++ scripts/mySql/s69.sql
@@ -1,31 +1,22 @@
+-- procedure with parameters
+
 use me;
 
-drop procedure if exists increase_coder_salary;
+drop procedure if exists get_coder_salary;
 
 delimiter //
 
-create procedure increase_coder_salary(
-	p_coder_id int(11),
-    p_increase decimal(8, 2)
+create procedure get_coder_salary(
+	in p_coder_id integer,
+    out p_salary decimal(8, 2)
 ) begin
-	declare v_error boolean default false;
-	declare continue handler for sqlexception
-		set v_error = true;
-
-	start transaction;
-
-	update coders
-    set salary = salary + p_increase
-    where coder_id = p_coder_id;
-    
-    if v_error then
-		select "can't update";
-        rollback;
-	else
-        commit;
-	end if;
+	select salary
+	into p_salary
+	from coders
+	where coder_id = p_coder_id;
 end;
 
 // delimiter ;
 
-call increase_coder_salary(104, 1);
+call get_coder_salary(104, @result);
+select @result;
